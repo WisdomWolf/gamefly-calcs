0268709f5b79caef0b74fc75e39c3a59a2c1e05c
WisdomWolf <wisdomwolf@gmail.com>
2017-08-18 10:33:09 -0500

    beginning migration to object oriented design

--- gamefly_calc.py
+++ gamefly_calc.py
@@ -4,29 +4,36 @@ from datetime import datetime, timedelta
 from dateutil.parser import parse as parse_date
 
 
-def get_daily_cost(monthly_cost):
-    return monthly_cost / 30
-    
-    
-def get_current_cost(daily_cost, rental_date):
+class GameRental(object):
+
+def __init__(self, rental_date, total_cost, monthly_fee=7):
     if not isinstance(rental_date, date):
         rental_date = parse_date(rental_date)
-    total_days = (datetime.today() - rental_date).days
-    return daily_cost * total_days
+    self.rental_date = rental_date
+    self.total_cost = total_cost
+    self.daily_cost = _get_daily_cost(monthly_fee)
     
-   
-def get_max_rental_time(daily_cost, total_cost):
+@property
+def current_cost(self):
+    return _get_current_cost()
+
+def _get_daily_cost(self, monthly_cost):
+    return monthly_cost / 30
+        
+def _get_current_cost(self):
+    total_days = (datetime.today() - self.rental_date).days
+    return self.daily_cost * total_days
+    
+def _get_max_rental_time(self, daily_cost, total_cost):
     return total_cost // daily_cost
     
-    
-def get_max_rental_date(max_time, rental_date=None):
+def _get_max_rental_date(self, max_time, rental_date=None):
     rental_date = rental_date or datetime.today()
     if not isinstance(rental_date, date):
         rental_date = parse_date(rental_date)
     return rental_date + timedelta(max_time)
     
-    
-def get_remaining_days(max_rental_date):
+def _get_remaining_days(self, max_rental_date):
     if not isinstance(max_rental_date, date):
         rental_date = parse_date(max_rental_date)
     return (max_rental_date - datetime.today()).days
