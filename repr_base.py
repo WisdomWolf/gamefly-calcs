554751b31d8c6725f3fde84c4613338b7571f22f
WisdomWolf <wisdomwolf@gmail.com>
2017-09-25 09:18:21 -0400

    added comments

--- repr_base.py
+++ repr_base.py
@@ -16,8 +16,9 @@ class ReprBase(object):
         
     @staticmethod
     def _sort(key):
+        '''Used to ensure certain attributes are listed first'''
         key = key.split(':')[0].lower()
-        sort_keys = ['id', 'name']
+        sort_keys = ['id', 'name'] # Should probably be a class variable so that it can be easily overridden
         try:
             return sort_keys.index(key)
         except ValueError:
